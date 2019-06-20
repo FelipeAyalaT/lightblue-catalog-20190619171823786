daebffa42138e3df4b294c609ffb5fd65d931a1e
FELIPE SANTOS AYALA TUME <fsayalat@hotmail.com>
2019-06-20 20:16:18 +0000

    s

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -28,7 +28,7 @@ public class CatalogController {
      */
     @RequestMapping(value = "/items", method = RequestMethod.GET)
     @ResponseBody
-    ResponseEntity<?> getInventory() {
+    Iterable<Inventory> getInventory() {
         return itemsRepo.findAll();
         //return ResponseEntity.ok("[{\"id\": 1,\"name\":\"one\"},{\"id\":2,\"name\":\"two\"}]");
     }
